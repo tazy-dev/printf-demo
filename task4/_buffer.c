1f3d4328e16b538bc9b36860f6feda1350e76fdb
tazy-dev <138940878+tazy-dev@users.noreply.github.com>
2023-09-20 01:42:41 +0200

    task 4

---
+++ task4/_buffer.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _putString - add string to the buffer
+ * 
+ * str : Input string
+ * Return : On success 1
+*/
+int _putString (char *str)
+{
+    int len = 0;
+    while (*str)
+    {
+        len++;
+        _putChar(*str++);
+    }
+    return (len);
+}
+/**
+ * _putChar - Add char to the buffer and print to console
+ *            when the buffer is full 
+ * 
+ * str : Input character
+ * Return : On success 1
+*/
+
+int _putChar(char c)
+{
+    static int i;
+    static char buffer[BUFFER_SIZE];
+    if (c == BUFFER_FLUSH || i >= BUFFER_SIZE)
+    {
+        write(1,buffer,i);
+        i = 0;
+    }
+    if (c != BUFFER_FLUSH)
+        buffer[i++] = c;
+    return (1);
+}
