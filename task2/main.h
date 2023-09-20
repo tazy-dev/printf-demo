e2f9a3a32cae65e77f0bf173441af5daf24a1407
tazy-dev <138940878+tazy-dev@users.noreply.github.com>
2023-09-20 21:59:10 +0200

    Task 2

--- task2/main.h
+++ task2/main.h
@@ -6,32 +6,48 @@
 #include <stdlib.h>
 #include <limits.h>
 
-
-
-#define BUFFER_FLUSH -1
 #define LOWERCASE 1
 #define UNSIGNED 2
 
-//main Functiond
-int _printf(const char *format,...);
+/**
+ * struct specifier - Different Printf Specifier
+ *
+ * @specifier : The specifier character.
+ * @function : A pointer to the method printing the value acoording
+ *              to the specifier and available flags
+ *
+ * Description : This structure handels the various
+ *               specifier and mapping each to a function that match
+ *               the specifier with arguments to print the result
+ *
+ */
+typedef struct specifier
+{
+	char  specifier;
+	int (*function)(va_list);/*Pointer to the Actual function*/
 
+} fmt_spec;
 
+/*main Functiond*/
+int _printf(const char *format, ...);
 
-// _buffer.c module
+/*_buffer.c module*/
 int _putString(char *);
-int _putChar(char );
+int _putChar(char);
 
-//print_functions.c file
-int print_char(char);
-int print_percentage();
-int print_string(char *str);
+/*print_functions.c file*/
+int print_char(va_list);
+int print_percentage(va_list);
+int print_string(va_list);
 
-//print_functions_radix.c file
-int print_bin(unsigned int );
-int print_int(long );
+/*print_functions_radix.c file*/
+int print_int(va_list);
+int print_bin(va_list);
 
-//utils.c file
+/*utils.c file*/
 
-char  *convert_number (long int , int ,int);
+char *convert_number(int, int, int);
+int (*function_pointer(char))(va_list);
+int sprcifier_function(char, va_list);
 
 #endif
--- task2/print_functions_radix.c
+++ task2/print_functions_radix.c
@@ -1,26 +1,27 @@
 #include "main.h"
 
 /**
- * print_int : Print integer to console
+ * print_int - Print integer to console
  *
- * @num : input number
- * Return : The number of digits of the output
+ * @ap : input argument
+ * Return: The number of digits of the output
  */
-int print_int(long num)
+int print_int(va_list ap)
 {
+	int num = va_arg(ap, int);
 
-    return (_putString(convert_number(num, 10, 0)));
+	return (_putString(convert_number(num, 10, 0)));
 }
 
 /**
- * print_bin : Print integer as Binary to console
+ * print_bin - Print integer as Binary to console
  *
- * @num : input number
- * Return : The number of digits of the output
+ * @ap : input argument
+ * Return: The number of digits of the output
  */
-int print_bin(unsigned int num)
+int print_bin(va_list ap)
 {
+	unsigned int num = va_arg(ap, unsigned int);
 
-    return (_putString(convert_number(num, 2, UNSIGNED)));
+	return (_putString(convert_number(num, 2, UNSIGNED)));
 }
-
